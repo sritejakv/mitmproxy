cadc01a9ef11365165e1d038c6fe8cf8fb7b8568
Maximilian Hils <git@maximilianhils.com>
2020-01-02 03:09:48 +0100

    [sans-io] fixes, fixes, fixes

--- mitmproxy/proxy/protocol/base.py
+++ mitmproxy/proxy/protocol/base.py
@@ -161,7 +161,7 @@ class ServerConnectionMixin:
         if not self.server_conn.address:
             raise exceptions.ProtocolException("Cannot connect to server, no server address given.")
         self.log("serverconnect", "debug", [repr(self.server_conn.address)])
-        self.channel.ask("serverconnect", self.server_conn)
+        self.channel._ask("serverconnect", self.server_conn)
         try:
             self.server_conn.connect()
         except exceptions.TcpException as e:
