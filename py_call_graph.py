959db9bc66d7004e0a84fcb246327ae642359c8d
Kummita Sriteja <sriteja.ku@gmail.com>
2020-01-24 16:56:30 +0100

    Adding pycallgraph files for test cases
    Adding pycallgraph files for test cases

---
+++ py_call_graph.py
@@ -0,0 +1,20 @@
+import pytest
+from pycallgraph import PyCallGraph
+from pycallgraph.output import GraphvizOutput
+from pycallgraph.config import Config
+from pycallgraph.globbing_filter import GlobbingFilter
+
+
+@pytest.fixture(autouse=True)
+def record_call_graph(request):
+    import time
+    current_millis = int(round(time.time() * 1000))
+    with PyCallGraph(output=GraphvizOutput(dot_file='./dotFiles/%s_%s.dot' % (request._pyfuncitem.name,
+                                                                              current_millis)),
+                     config=Config(debug=True,
+                                   trace_filter=GlobbingFilter(
+                                       exclude=['pycallgraph.*', 'test.*', '*.yml', "_pytest.*", "numpy.*"],
+                                       include=['mitmproxy.*']
+                                   )
+                                   )):
+        yield
