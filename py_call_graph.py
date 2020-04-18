3002b7e2af18999a8fc7ce193567feb3f3938dbc
Kummita Sriteja <sriteja.ku@gmail.com>
2020-04-18 21:05:44 +0200

    Adding entry points to the dynamic CG

--- py_call_graph.py
+++ py_call_graph.py
@@ -9,11 +9,22 @@ from pycallgraph.globbing_filter import GlobbingFilter
 def record_call_graph(request):
     import time
     current_millis = int(round(time.time() * 1000))
+    total_input_files = set()
+    with open("../entry_point_extractor/commonEntryPoints/mitmproxy.txt") as f:
+        for line in f:
+            temp = line.split("mitmproxy")[-1][1:]
+            temp = temp.replace("/", ".")
+            temp = temp.replace(".py", ".*")
+            temp = temp.replace("\n", "")
+            cur_input = "."
+            if len(temp) > 0:
+                total_input_files.add("mitmproxy." + temp)
+
     with PyCallGraph(output=GraphvizOutput(dot_file='./dotFiles/%s.dot' % current_millis),
                      config=Config(debug=True,
                                    trace_filter=GlobbingFilter(
-                                       exclude=['pycallgraph.*', 'test.*', '*.yml', "_pytest.*", "numpy.*"],
-                                       include=['mitmproxy.*']
+                                       include=list(total_input_files),
+                                       exclude=['pycallgraph.*', 'test*.*', '*.tests.*', '*test.*']
                                    )
                                    )):
         yield
